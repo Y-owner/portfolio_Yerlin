cd080ba4c163f5d54aaffcb0ec9a4110e41d02e8
Y-owner <davilayerlin15@gmail.com>
2026-05-12 12:44:41 -0600

    Primer avance del portafolio con estructura y navegación

---
+++ js/tareas.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  let menu = document.getElementById("sidebar");
+   let boton = document.getElementById("btn");
+  menu.classList.toggle("active");
+  boton.classList.toggle("move");
+}
+
+
