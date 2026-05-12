f5e31e3a14742035979dc0a79fade99633a4a2fa
Y-owner <davilayerlin15@gmail.com>
2026-05-12 12:56:12 -0600

    fix: mover archivos fuera de carpeta JS

--- js/script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-  let menu = document.getElementById("sidebar");
-   let boton = document.getElementById("btn");
-  menu.classList.toggle("active");
-  boton.classList.toggle("move");
-}
